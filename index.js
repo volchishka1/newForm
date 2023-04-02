cd259ac2401ad9cbfd1e2fa6296311ca2aba5757
macwolf <romazverb18@gmsil.com>
2023-04-02 18:58:26 +0300

    add function firstName and LastName

--- index.js
+++ index.js
@@ -26,3 +26,39 @@ function validationForm()
     }
     return false;
 }
+
+function firstName_validation(firstName, min, max)
+{
+    let lastNameLength = firstName.value.length;
+    let letters = /^[A-Za-z]+$/;
+    if(firstName.value.match(letters))
+    {
+        return true;
+    } else if (lastNameLength === 0 || lastNameLength >= min || lastNameLength < max) {
+        alert('Имя должна содержать от 2 до 25 символов')
+    }
+    else
+    {
+        alert('Имя содержит недопустимые символы и должно содержать не менее 2 и не более 25 символов');
+        firstName.focus();
+        return false;
+    }
+}
+
+function lastName_validation(lastName, min, max)
+{
+    let lastNameLength = lastName.value.length;
+    let letters = /^[A-Za-z]+$/;
+    if(lastName.value.match(letters))
+    {
+        return true;
+    } else if (lastNameLength === 0 || lastNameLength >= min || lastNameLength < max) {
+        alert('Фамилия должна содержать от 2 до 25 символов')
+    }
+    else
+    {
+        alert('Фамилия содержит недопустимые символы');
+        lastName.focus();
+        return false;
+    }
+}
